ca0be90a5f4d4d0bbda3fbe1df7c308470ac5f94
benlgw <ben.greaveswarwick@tutamail.com>
2026-06-26 15:30:11 +0100

    add the test code specified in requirements

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 //ts-worksheet-with-variables
 import LinkedList from "./modules/LinkedList.js";
 
-const test = new LinkedList();
+// example uses class syntax - adjust as necessary
+const list = new LinkedList();
+
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+
+list.toString();
